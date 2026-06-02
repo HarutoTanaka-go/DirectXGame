a5a9134e437a7861ba614066e56854fe148bf9bb
HarutoTanaka-go <k024g0048@g.neec.ac.jp>
2026-06-02 15:54:02 +0900

    okokokoko

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -220,6 +220,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	return 0;
 }
 
+
 // シェーダーコンパイル関数
 //  filePath    : シェーダーファイルのパス    例) L"Resources/shaders/TestVS.hlsl"
 //  shaderModel : シェーダーモデル          例) "vs_5_0"
